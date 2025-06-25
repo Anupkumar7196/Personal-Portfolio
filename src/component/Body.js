b7879b93e11afc2f9c7731718b594339dd97cdd8
Anup kumar <anupk7196@gmail.com>
2025-06-25 06:53:21 +0530

    update

--- src/component/Body.js
+++ src/component/Body.js
@@ -291,7 +291,7 @@ const Body = () => {
       {/* Featured Projects */}
       <section className="bg-gradient-to-r from-slate-900 via-slate-800 to-slate-900 py-16">
         <div className="container mx-auto px-6">
-          <h2 className="text-4xl font-bold text-center mb-12 text-white">
+          <h2 className="text-4xl font-bold text-center mb-12 bg-gradient-to-r from-white via-blue-100 to-blue-200 bg-clip-text text-transparent ">
             Featured Projects
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
@@ -352,7 +352,7 @@ const Body = () => {
       {/* Skills */}
       <section className="py-16 bg-gradient-to-r from-slate-900 via-slate-800 to-slate-900">
         <div className="container mx-auto px-6 text-center">
-          <h2 className="text-4xl font-bold mb-12 text-white">Tech Stack</h2>
+          <h2 className="text-4xl font-bold mb-12 bg-gradient-to-r from-white via-blue-100 to-blue-200 bg-clip-text text-transparent ">Tech Stack</h2>
           <div className="grid grid-cols-2 md:grid-cols-6 gap-4 cursor-pointer ">
             {[
               "Java",
@@ -365,14 +365,14 @@ const Body = () => {
               <div
                 key={skill}
                 className="bg-gradient-to-r from-gray-900 via-blue-900 to-gray-900
-                 p-6 rounded-lg shadow-lg hover:shadow-xl  transition duration-300 ease-in-out hover:scale-95 hover:bg-transparent-100 cursor-pointer"
+                 p-6 rounded-lg shadow-lg hover:shadow-xl  transition duration-300 ease-in-out hover:scale-95 hover:bg-transparent-100 text-gray-700 cursor-pointer"
               >
                 <div className="w-16 h-16 bg-blue-100 rounded-full flex items-center justify-center mx-auto mb-4">
                   <span className="text-2xl font-bold text-blue-600">
                     {skill[0]}
                   </span>
                 </div>
-                <h3 className="font-semibold text-gray-800">{skill}</h3>
+                <h3 className="font-semibold text-gray-300">{skill}</h3>
               </div>
             ))}
           </div>
