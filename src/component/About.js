c916bf28a4bb6553b350c38e8ed79cc58a318d8d
Anup kumar <anupk7196@gmail.com>
2025-07-01 19:06:30 +0530

    update resume

--- src/component/About.js
+++ src/component/About.js
@@ -17,7 +17,7 @@ const About = () => {
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center mb-16">
           <div className="flex-shrink-0 flex justify-center items-center">
             <img
-              src="https://github.com/Anupkumar7196/Personal-Portfolio/blob/main/src/assets/IMG_4582.JPG?raw=true"
+              src="https://github.com/Anupkumar7196/Personal-Portfolio/blob/main/src/assets/WhatsApp%20Image%202025-06-24%20at%2022.52.33.jpeg?raw=true"
               alt="Anup kumar"
               className="w-100 h-130 rounded-lg object-cover border-4 border-gray-800 shadow-2xl items-cente"
             />
